c3fe1a517f38258aa14936b4c447549ba18cd965
Unknown <wang7186wang@gmail.com>
2018-05-14 21:06:56 +0800

    Revert "add xray"
    This reverts commit 05d716776247f7f343492e903e82008fc3426f02.

--- index.js
+++ index.js
@@ -1,12 +1,1 @@
-const Xray = require('x-ray'); // 路徑下的檔案使用 require(./name.js)
-const x = Xray();
-
-
-x ('https://kiki.ccu.edu.tw/~ccmisp06/Course/5304.html','table > tr',[
-    {
-    NO:'td:nth-child(2)',
-    courseName:'td:nth-child(4)',
-    teacher:'td:nth-child(5)',
-    }
-]).write('./dist/course.json');
-
+console.log('hello');
